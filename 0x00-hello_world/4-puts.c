e270c5a42c105f5637ca3f246022d281ca9f0a91
Mohamed Ala <alaaouiti96@gmail.com>
2023-02-16 14:58:20 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-int main (void){
-	char Mystr[]="\"Programming is like building a multilingual puzzle";
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(Mystr);
-	return 0;
+	return (0);
 }
