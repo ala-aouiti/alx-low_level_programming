5361daa39b029307fbf9e7f43d8280bcd5741dc4
Mohamed Ala <alaaouiti96@gmail.com>
2023-02-16 15:34:03 -0800

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: Value 0 always
+ */
+int main(void)
+{
+	printf("Size of a char: %2d byte(s)\n", sizeof(char));
+	printf("Size of an int: %2d byte(s)\n", sizeof(int i));
+	return (0);
+}
