65cd01b0cbfbb1001ef41563202316eadb732500
Mohamed Ala <alaaouiti96@gmail.com>
2023-02-19 14:39:39 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *  * main - this is the main function
+ *  *
+ *  * Return: Value 0 always
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d",a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
