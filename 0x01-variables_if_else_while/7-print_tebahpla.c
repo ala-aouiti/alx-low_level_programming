505693ea32e6cd1f1553f3d3d6841320a640da5b
Mohamed Ala <alaaouiti96@gmail.com>
2023-02-19 14:53:01 -0800

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *  * main - this is the main function
+ *  *
+ *  * Return: Value 0 always
+ */
+int main(void)
+{
+	char alph = 'z';
+
+	while (alph >= 'a')
+	{
+		putchar(alph);
+		alph--;
+	}
+	putchar('\n');
+	return (0);
+}
