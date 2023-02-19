077921552bd1ee96a6495f3284d771f6e18f3905
Mohamed Ala <alaaouiti96@gmail.com>
2023-02-19 14:03:27 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *  * main - this is the main function
+ *  *
+ *  * Return: Value 0 always
+ */
+int main(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	putchar('\n');
+	return (0);
+}
