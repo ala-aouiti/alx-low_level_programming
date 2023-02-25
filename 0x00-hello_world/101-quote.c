4d55af0ab42473c525bf04b0b5991a5abfdb4547
Mohamed Ala <alaaouiti96@gmail.com>
2023-02-25 06:28:00 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	int i;
-	char s[80] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	i = 0;
 
