8cf84d8db211f99a4fa69da3d6b8d7c36cfa9f04
Mohamed Ala <alaaouiti96@gmail.com>
2023-02-26 13:15:53 -0800

    0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *  * main - this is the main function
+ *  *
+ *  * Return: Value 0 always
+ */
+int main(void)
+{	
+	char c = 'a';
+
+	while(c <= 'z')
+	{
+		printf("c\n");
+		c++;
+	}
+	return (0);
+
+}
