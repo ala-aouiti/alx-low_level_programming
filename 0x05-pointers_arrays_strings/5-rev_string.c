bbe6060f308e91c4b645319ee4aba220bb3c7fab
Mohamed Ala <alaaouiti96@gmail.com>
2023-02-27 18:58:55 -0800

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - string in reverse
+ * @s: parameter pointer of type char
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = strlen(s); i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
