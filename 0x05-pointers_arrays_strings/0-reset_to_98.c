cfaa0f0ff188cdabc84f5057655ac886eb6b2e19
Mohamed Ala <alaaouiti96@gmail.com>
2023-02-27 17:07:21 -0800

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,16 +2,12 @@
 
 /**
  * reset_to_98 - takes a pointer to an int as param and ..
- * n: parameter of type int
+ * @n: parameter of type int
  *
  * Return: void
  */
 
 void reset_to_98(int *n)
 {
-	int x;
-
-	n = &x;
 	*n = 98;
-	x = 98;
 }
