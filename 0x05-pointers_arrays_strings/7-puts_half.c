da164d13757442878c066654e6628831b3fe7fcb
Mohamed Ala <alaaouiti96@gmail.com>
2023-02-28 15:09:52 -0800

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,14 +16,14 @@ void puts_half(char *str)
 
 	if (l%2)
 	{
-		for (i = l/2; i <= l; i++)
+		for (i = l/2; i < l; i++)
 		{
 			_putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = n+1; i <= l; i++)
+		for (i = n+1; i < l; i++)
 		{
 			_putchar(str[i]);
 		}
