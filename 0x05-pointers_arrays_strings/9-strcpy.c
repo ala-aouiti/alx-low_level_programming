85590ac4e8a818742a707aed57e4fb8d47763dd6
Mohamed Ala <alaaouiti96@gmail.com>
2023-02-28 16:39:41 -0800

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,16 +14,13 @@ char *_strcpy(char *dest, char *src)
 	int i;
 	int l = strlen(src);
 
-	for (i = 0; i < l; i++)
+	for (i = 0; i < l && src[i] != '\0'; i++)
 	{
-		if (src[i] != '\0')
-		{
 			dest[i] = src[i];
-		}
-		else
-		{
-			dest[i] = '\0';
-		}
+	}
+	for (i = l;; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
