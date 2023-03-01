11ae3c6034dc41e5af43af2c32252d24f627f014
Mohamed Ala <alaaouiti96@gmail.com>
2023-02-28 16:26:02 -0800

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copy src to dest
+ * @dest: destination string and the return
+ * @src: the string to be copied
+ *
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int l = strlen(src);
+
+	for (i = 0; i < l; i++)
+	{
+		if (src[i] != '\0')
+		{
+			dest[i] = src [i];
+		}
+		else
+		{
+			dest[i] = '\0';
+		}
+	}
+}
